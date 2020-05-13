bd8a0e79e83e2ac52a65e997c7c874fa0b396459
Ashish Padhi <ashish.padhi@bulkmro.com>
2020-05-13 14:22:50 +0530

    usage of lists and keys handled.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -7,63 +7,67 @@ class App extends Component {
   state = {
     person: [
       {
+        id: 1290,
         name: 'Niharika',
         age: 22
       },
       {
+        id: 1926,
         name: 'Nainakshi',
         age: 22
       },
       {
+        id: 8751,
         name: 'Prateeti',
         age: 23
       }
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPerson: false
   }
 
-  switchNameHandler = (newName) => {
-    // console.log('was clicked!!!')
-    // DON'T DO THIS
-    // this.state.person[0].name = 'Jhalli'
-  
+  deletePersonHandler = (personIndex) => {
+
+    // const persons = this.state.person;
+    // const persons = this.state.person.slice();
+    const persons = [...this.state.person]
+
+    persons.splice(personIndex, 1);
+
     this.setState({
-      person: [
-        {
-          name: 'Varsha',
-          age: 22
-        },
-        {
-          name: 'Sakshi',
-          age: 22
-        },
-        {
-          name: newName,
-          age: 23
-        }
-      ]
+      person: persons
     })
   }
 
-  nameChangeHandler = event => {
+  nameChangeHandler = (event, id) => {
 
-    event.preventDefault()
+    const personIndex = this.state.person.findIndex(p => {
+      return p.id === id
+    })
+
+    const person = {
+      ...this.state.person[personIndex]
+    }
+
+    // const personAlt = Object.assign({}, this.state.person[personIndex] )
+
+    person.name = event.target.value 
+
+    const persons = [...this.state.person]
+
+    persons[personIndex] = person
+     
+    this.setState({
+      person: persons
+    })
+  }
+
+  togglePersonsHandler = () => {
+
+    const doesShow = this.state.showPerson
 
     this.setState({
-      person: [
-        {
-          name: 'Niharika',
-          age: 22
-        },
-        {
-          name: 'Nainakshi',
-          age: 22
-        },
-        {
-          name: event.target.value,
-          age: 23
-        }
-      ]
+      showPerson: !doesShow
     })
   }
 
@@ -79,26 +83,42 @@ class App extends Component {
       cursor: 'pointer'
     }
 
+    let persons = null
+
+    if(this.state.showPerson) {
+      persons = (
+        <div>
+          {this.state.person.map((p, i) => {
+            return <Person 
+              click={this.deletePersonHandler.bind(this, i)}
+              name={p.name} 
+              age={p.age} 
+              key={p.id}
+              change={(event) => this.nameChangeHandler(event, p.id)}
+             />
+          })}
+          {/* <Person 
+            name={this.state.person[1].name} 
+            age={this.state.person[1].age} 
+            // How to pass argument to method using bind() and 'this'
+            click={this.switchNameHandler.bind(this, 'MeenSn')} // passed the methods reference to the component
+            change={this.nameChangeHandler}
+          >
+            My hobbies: Painting
+          </Person> */}
+        </div>
+      )      
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App!!</h1>
         <p>This is really working.</p>
         <button 
           style={style}
-          // Second method of passing argument
-          onClick={() => this.switchNameHandler('Meenakshi')} 
-        >Switch Names</button>
-        <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
-        <Person 
-          name={this.state.person[1].name} 
-          age={this.state.person[1].age} 
-          // How to pass argument to method using bind() and 'this'
-          click={this.switchNameHandler.bind(this, 'MeenSn')} // passed the methods reference to the component
-          change={this.nameChangeHandler}
-        >
-          My hobbies: Painting
-        </Person>
-        <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
+          onClick={this.togglePersonsHandler} 
+        >{ !this.state.showPerson ? 'Show Persons': 'Hide Persons'}</button>
+        {persons}
       </div>
     );
   }
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -3,10 +3,10 @@ import './Person.css'
 
 const Person = (props) => {
     return (
-        <div className="Person">
+        <div className="Person" >
             <p onClick={props.click}> I'm {props.name} and I am {props.age} year old! </p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.change} />
+            <input type="text" onChange={props.change} defaultValue={props.name}/>
         </div>
     )
     
