24de551fec547d5dada50cd84184ea72fbbd3c89
Ashish Padhi <ashish.padhi@bulkmro.com>
2020-05-15 18:01:18 +0530

    Learned about constructor.

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,15 +1,26 @@
 import React from 'react'
+import './Cockpit.scss'
 
 const Cockpit = (props) => {
+
+    let classes = []
+
+    if(props.personLength <= 2) {
+      classes.push('red')
+    }
+    if(props.personLength <= 1) {
+      classes.push('bold')
+    }
+
     return (
-        <div>
-            <h1>Hi, I'm a React App!!</h1>
-            <p className={props.redNBoldLine.join(' ')}>This is really working.</p>
+        <div className="Cockpit">
+            <h1>Hi, I'm a {props.title}</h1>
+            <p className={classes.join(' ')}>This is really working.</p>
             <button
-                className={props.btnClass}
+                className={ props.showPerson ? 'greenButton': 'Button' }
                 //hover={this.state.showPerson}
-                onClick={this.togglePersonsHandler} >
-                { !this.state.showPerson ? 'Show Persons': 'Hide Persons'}
+                onClick={props.clicked} >
+                { !props.showPerson ? 'Show Persons': 'Hide Persons' }
             </button>
         </div>
     )
--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -7,32 +7,23 @@ import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
 
-  state = {
-    person: [
-      {
-        id: 1290,
-        name: 'Niharika',
-        age: 22
-      },
-      {
-        id: 1926,
-        name: 'Nainakshi',
-        age: 22
-      },
-      {
-        id: 8751,
-        name: 'Prateeti',
-        age: 23
-      }
-    ],
-    otherState: 'some other value',
-    showPerson: false
+  constructor(props) {
+    super(props);
+    consol.log('[App.js] constructor');
+
+    this.state = {
+      person: [
+        { id: 1290, name: 'Niharika', age: 22 },
+        { id: 1926, name: 'Nainakshi', age: 22 },
+        { id: 8751, name: 'Prateeti', age: 23 }
+      ],
+      otherState: 'some other value',
+      showPerson: false
+    }
   }
 
   deletePersonHandler = (personIndex) => {
 
-    // const persons = this.state.person;
-    // const persons = this.state.person.slice();
     const persons = [...this.state.person]
 
     persons.splice(personIndex, 1);
@@ -52,14 +43,12 @@ class App extends Component {
       ...this.state.person[personIndex]
     }
 
-    // const personAlt = Object.assign({}, this.state.person[personIndex] )
-
-    person.name = event.target.value 
+    person.name = event.target.value
 
     const persons = [...this.state.person]
 
     persons[personIndex] = person
-     
+
     this.setState({
       person: persons
     })
@@ -77,38 +66,25 @@ class App extends Component {
   render() {
 
     let persons = null
-    let btnClass = 'button'
 
     if(this.state.showPerson) {
       persons = (
         <div>
-          <Persons 
+          <Persons
             person={this.state.person}
             clicked={this.deletePersonHandler}
             changed={this.nameChangeHandler}
-          /> 
+          />
           {/* Key always has to be in the outer element in a map method */}
         </div>
-      )      
-
-      btnClass = 'greenButton'
-      // style.backgroundColor = 'green'
-    }
-
-    let classes = []
-
-    if(this.state.person.length <= 2) {
-      classes.push('red')
-    } 
-    if(this.state.person.length <= 1) {
-      classes.push('bold')
+      )
     }
 
     return (
       <div className='App'>
-        <Cockpit 
-          redNBoldLine={classes}
-          btnClass={btnClass}
+        <Cockpit
+          title={this.props.appTitle}
+          personLength={this.state.person.length}
           clicked={this.togglePersonsHandler}
           showPerson={this.state.showPerson}
         />
@@ -116,9 +92,6 @@ class App extends Component {
       </div>
     );
   }
-  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
 }
 
 export default App;
-
-
