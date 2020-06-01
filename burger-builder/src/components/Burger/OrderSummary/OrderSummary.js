c9217ed32b36f6ea890c521831eacf392bbfafbb
Ashish Padhi <ashish.padhi@bulkmro.com>
2020-06-01 23:34:25 +0530

    changed the folder structure of the App.

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,37 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 import Button from '../../UI/Button/Button'
 
-const OrderSummary = (props) => {
+class OrderSummary extends Component {
 
-    const ingredientSummary = Object.keys(props.ingredients).map(igKeys => {
-        return (<li key={igKeys}> 
-                    <span 
-                        style={{textTransform: 'capitalize'}} >
-                        {igKeys}
-                    </span> : {props.ingredients[igKeys]}
-                </li> )
-    })
+    // This could be functional component, doesn't have to be a class.
+    
+    render() {
 
-    return (
-        <Fragment>
-            <h3>Your Order</h3>
-            <p>A delicious Burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p> <b>Total Price: {props.price}</b> </p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.cancelOrder}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.continueOrder}>CONTINUE</Button>
-        </Fragment>
-    )
+        const { ingredients, price, cancelOrder, continueOrder } = this.props
+
+        const ingredientSummary = Object.keys(ingredients).map(igKeys => {
+            return (<li key={igKeys}> 
+                        <span 
+                            style={{textTransform: 'capitalize'}} >
+                            {igKeys}
+                        </span> : {ingredients[igKeys]}
+                    </li> )
+        })
+    
+        return (
+            <Fragment>
+                <h3>Your Order</h3>
+                <p>A delicious Burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p> <b>Total Price: {price}</b> </p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={cancelOrder}>CANCEL</Button>
+                <Button btnType="Success" clicked={continueOrder}>CONTINUE</Button>
+            </Fragment>
+        )
+    }
 }
 
 export default OrderSummary
--- burger-builder/src/components/UI/Modal/Modal.js
+++ burger-builder/src/components/UI/Modal/Modal.js
@@ -1,22 +1,36 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 
 import './Modal.scss'
 import Backdrop from '../Backdrop/Backdrop'
 
-const Modal = (props) => {
-    return (
-        <Fragment>
-            <div 
-                className="Modal"
-                style={{ 
-                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.show ? '1' : '0'
-                }}>
-                {props.children}
-            </div>
-            <Backdrop show={props.show} clicked={props.modalClosed} />
-        </Fragment>
-    )
+class Modal extends Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show
+    }
+
+    componentDidUpdate() {
+        console.log('[Modal.js] did update')
+    }
+
+    render() {
+
+        const { show, children, modalClosed } = this.props
+
+        return (
+            <Fragment>
+                <div 
+                    className="Modal"
+                    style={{ 
+                        transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: show ? '1' : '0'
+                    }}>
+                    {children}
+                </div>
+                <Backdrop show={show} clicked={modalClosed} />
+            </Fragment>
+        )
+    }
 }
 
 export default Modal
